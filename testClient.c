e992ecb3d579c5bc040689e249c1d0571f55a19f
tvanfossen <tristan.vanfossen@yahoo.com>
2018-01-31 10:58:33 -0500

    Add files via upload

--- testClient.c
+++ testClient.c
@@ -2,6 +2,7 @@
  * Client that sends data to a server and then receives it back.
  *
  * @author Joshua Crum
+ * @author Tristan VanFossen
  */
 
 #include <arpa/inet.h>
@@ -11,93 +12,129 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/uio.h>
 
+#pragma pack(1)
+struct packet {
+	int p_num;
+	char buffer[1024];
+};
+#pragma pack(0)
 
 
 int main(int argc, char **argv)
 {
-    const int window_size = 5;
-    int port_num;
-    char temp[5];
-    char client_ip[16];
-    
-    printf("Enter an IP address: ");
-    fgets(client_ip, 16, stdin);
-    printf("Enter a port number: ");
-    fgets(temp, 5, stdin);
-    port_num = atoi(temp);
-    
-    struct timeval timeout;
-    timeout.tv_sec = 2;
-    timeout.tv_usec = 0;
+	int port_num;
+	char temp[5];
+	char client_ip[16];
 
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-    struct sockaddr_in serveraddr;
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(port_num);
-    serveraddr.sin_addr.s_addr = inet_addr(client_ip);
-    
-    printf("Enter a file name: ");
-    char fname[32];
-    
-    /* Flush stdin. */
-    char tmp_name[16];
-    fgets(tmp_name, 16, stdin);
-    fgets(fname, 32, stdin);
-    
-    /* Remove trailing newline. */
-    int l = (int)strlen(fname);
-    if (fname[l - 1] == '\n') {
-        fname[l - 1] = '\0';
-    }
-    
-    socklen_t len = sizeof(serveraddr);
-    
-    /* Send file name request to server. */
-    sendto(sockfd, fname, strlen(fname) + 1, 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
-    
-    /* Receive contents of file (if it exists) from server. */
+	printf("Enter an IP address: ");
+	fgets(client_ip, 16, stdin);
+	printf("Enter a port number: ");
+	fgets(temp, 5, stdin);
+	port_num = atoi(temp);
+
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	struct sockaddr_in serveraddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(port_num);
+	serveraddr.sin_addr.s_addr = inet_addr(client_ip);
+
+	printf("Enter a file name: ");
+	char fname[32];
+
+	/* Flush stdin. */
+	char tmp[16];
+	fgets(tmp, 16, stdin);
+	fgets(fname, 32, stdin);
+
+	/* Remove trailing newline. */
+	int l = (int)strlen(fname);
+	if (fname[l - 1] == '\n') {
+		fname[l - 1] = '\0';
+	}
+
+	printf("\nYou requested the file: %s\n", fname);
+
+	socklen_t len = sizeof(serveraddr);
+
+	/* Send file name request to server. */
+	sendto(sockfd, fname, strlen(fname) + 1, 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	int packet_info[4];
+	int packet_num = 0;
+	
+	recvfrom(sockfd, packet_info, sizeof(int)*4+1, 0, (struct sockaddr*) &serveraddr, &len);
+	printf("%d packet #, %d bytes, %d total packets, %d window size\n",
+		packet_info[0], packet_info[1], packet_info[2], packet_info[3]);
+
+	char** packet_storage = (char**)malloc(sizeof(char)*packet_info[2]);
+	char temp_buf[1024];
+	struct packet msg;
+	FILE* file = fopen ("temp.pdf", "wb");
+	
+	if (packet_info[1] > 1024)
+	{
+		while (packet_num <= packet_info[2])
+		{
+			long x = recvfrom(sockfd, &msg, sizeof(struct packet), 0, (struct sockaddr*) &serveraddr, &len);
+			
+			if (packet_num == msg.p_num)
+			{
+				packet_num++;
+				sendto(sockfd, &msg.p_num, sizeof(int) + 1, 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+				packet_storage[msg.p_num] = msg.buffer;
+			}
+			else if (x == -1)
+			{
+				break;
+			}
+			else
+			{
+				sendto(sockfd, &packet_num, sizeof(int) + 1, 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+			}
+				
+		}
+		long n = recvfrom(sockfd, &msg, sizeof(struct packet), 0, (struct sockaddr*) &serveraddr, &len);
+		if (n == -1)
+		{
+			packet_num++;
+			sendto(sockfd, &msg.p_num, sizeof(int) + 1, 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+			packet_storage[msg.p_num] = msg.buffer;
+		}
+		}
+		
+	else
+	{
+		printf("Packet less than 1024\n");
+	}
+	for (int i = 0; i < packet_info[2];i++)
+	{
+		fwrite(packet_storage[i], sizeof(char), sizeof(packet_storage[i]), file);
+	}
+	fclose(file);
+	close(sockfd);
+	return 0;
+}
+
+
+/*
+
     printf("Enter a new file name with proper extension: ");
     char new_file[32];
     fgets(new_file, 32, stdin);
     
-    /* Remove trailing newline. */
+
     int nl = (int)strlen(new_file);
     if (new_file[nl - 1] == '\n') {
         new_file[nl - 1] = '\0';
     }
 
-    /* Create a new file with user input. */
+
     FILE *file = fopen(new_file, "wb");
-    char tmp[1024];
-        
-    /* Read byte array from server to buffer. */
-    int i = 1;
-    int i_size = 32 * sizeof(char);
-    char *ack = (char*) malloc(3 * sizeof(char));
-    char *initial = (char*) malloc(i_size);
-    recvfrom(sockfd, initial, i_size, 0, (struct sockaddr*) &serveraddr, &len);
+    char *buffer = (char*) malloc (1024 * sizeof(char));
     
-    char *s_num_packets, *s_file_length;
-    s_num_packets = strtok(initial, "/");
-    s_file_length = strtok(NULL, "/");
-    int num_packets = atoi(s_num_packets);
-    int file_length = atoi(s_file_length);
-        
-    free(initial);
-    
-    ssize_t bytes_recv;
-    
-    while((bytes_recv = recvfrom(sockfd, tmp, 1024, 0, (struct sockaddr*) &serveraddr, &len)) != -1) {
-        sprintf(ack,"%d", i);
-        fwrite(tmp, 1, bytes_recv, file);
-        i++;
-        sendto(sockfd, ack, 4, 0, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
-    }
-    
-    free(ack);
+    recvfrom(sockfd, buffer, 1000000, 0, (struct sockaddr*) &serveraddr, &len);
+    int buf_size = 1000000;
+    fwrite(buffer, 1, buf_size, file);
     fclose(file);
-    close(sockfd);
-    return 0;
-}
+    */
--- testServer.c
+++ testServer.c
@@ -2,6 +2,7 @@
  * UDP Server that echos client input.
  *
  * @author Joshua Crum
+ * @author Tristan VanFossen
  */
 
 #include <netinet/in.h>
@@ -11,82 +12,121 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include "file_utils.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-
+#pragma pack(1)
+struct packet {
+	int p_num;
+	char buffer[1024];
+};
+#pragma pack(0)
 
 int main(int argc, char **argv)
 {
-    const int window_size = 5;
-    const int max_packet_size = 1024;
-    int port_num;
-    char temp[5];
-    
-    printf("Enter a port number: ");
-    fgets(temp, 5, stdin);
-    port_num = atoi(temp);
-    
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    
-    struct timeval timeout;
-    timeout.tv_sec = 5;
-    timeout.tv_usec = 0;
-    
-    /* Set socket options. */
-    setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-    struct sockaddr_in serveraddr, clientaddr;
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(port_num);
-    serveraddr.sin_addr.s_addr = INADDR_ANY;
-    bind(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
-    
-    while (1) {
-        socklen_t len = sizeof(clientaddr);
-        char fname[32];
-        long n = recvfrom(sockfd, fname, 32, 0, (struct sockaddr*) &clientaddr, &len);
-        
-        /* Checks for any recv error, not just timeout. */
-        if (n == -1) {
-            printf("Timed out while waiting to receive.\n");
-        }
-        else {
-            printf("File request from client: %s\n", fname);
-            /* Check if file exists and then send its contents. */
-            if (access(fname, F_OK) != -1) {
-                FILE *file = fopen(fname, "rb");
+	int port_num;
+	char temp[5];
+
+	printf("Enter a port number: ");
+	fgets(temp, 5, stdin);
+	port_num = atoi(temp);
+
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	struct timeval timeout;
+	timeout.tv_sec = 5;
+	timeout.tv_usec = 0;
+
+	/* Set socket options. */
+	setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+	struct sockaddr_in serveraddr, clientaddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons(port_num);
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
+	bind(sockfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+
+	while (1) {
+		socklen_t clen = sizeof(clientaddr);
+		char fname[32];
+		long n = recvfrom(sockfd, fname, 32, 0, (struct sockaddr*) &clientaddr, &clen);
+
+		/* Checks for any recv error, not just timeout. */
+		if (n == -1) {
+			printf("Timed out while waiting to receive.\n");
+		}
+		else {
+			printf("File request from client: %s\n", fname);
+
+			if (access(fname, F_OK) != -1) {
+
+				FILE *file = fopen(fname, "rb");
+				struct stat st;
+				int len;
+
+				if(stat(fname, &st) == 0) {
+					len = st.st_size;
+				}
+				char temp_buf [1024];
+				int packet_num = 0;
+				int window_size = 5;
+				int packet_info [4] = {-1, len, len/1024, window_size};
+				struct packet msg;
+
+				//printf("%s contains %d bytes for %d packets\n", fname, len, len/1024+1);
+				sendto(sockfd, packet_info, sizeof(int)*4, 0, (struct sockaddr*)&clientaddr, sizeof(clientaddr));
+				if (packet_info[1] > 1024)
+				{
+					while (packet_num <= packet_info[2])
+					{
+						
+						for (int i = packet_num; i < packet_num + window_size; i++)
+						{
+							msg.p_num = i;
+							fseek(file, packet_num*1024, SEEK_SET);
+							fread(msg.buffer, sizeof(msg.buffer), 1024, file);
+							
+							sendto(sockfd,&msg,sizeof(struct packet)+1,0,(struct sockaddr*)&clientaddr,sizeof(clientaddr));
+						}
+						n = recvfrom(sockfd, &packet_num, sizeof(int), 0, (struct sockaddr*) &clientaddr, &clen);
+						printf("%d\n", packet_num);
+						if (n==-1)
+						{
+							fseek(file, packet_info[2]*1024, SEEK_SET);
+							fread(msg.buffer, sizeof(msg.buffer), len-packet_info[2]*1024, file);
+							sendto(sockfd,&msg,sizeof(struct packet)+1,0,(struct sockaddr*)&clientaddr,sizeof(clientaddr));
+							break;
+						}
+						
+					}
+				}
+				else
+				{
+					fread(temp_buf, 1024, packet_info[1], file);
+					sendto(sockfd, temp_buf, 1024+1, 0, (struct sockaddr*)&clientaddr, sizeof(clientaddr));
+				}
+				fclose(file);
+			}
+			else {
+				printf("The file '%s' could not be found.\n", fname);
+			}
+			
+		}
+	}
+	return 0;
+	}
+
+
+
+ /*
+		FILE *file = fopen(fname, "rb");
                 
-                /* Get file length. */
                 fseek(file, 0, SEEK_END);
-                long file_len = ftell(file);
+                long len = ftell(file);
                 rewind(file);
                 
-                double r = ((double) file_len / 1024) + 1;
-                char tmp[1024];
-                char ack[3];
+                char *buffer = (char*) malloc ((len + 1) * sizeof(char));
+                fread(buffer, len, 1, file);
+                fclose(file);
                 
-                /* Send initial size packet. */
-                double num_packets = file_len / max_packet_size;
-                char *initial = (char*) malloc (file_len + sizeof(num_packets));
-                sprintf(initial, "%d", (int)num_packets);
-                strcat(initial, "/");
-                sprintf(initial + strlen(initial), "%ld", file_len);
-                sendto(sockfd, initial, (strlen(initial) * sizeof(char)), 0, (struct sockaddr*) & clientaddr, sizeof(clientaddr));
-                printf("Initial :%s:\n", initial);
-                free(initial);
-                
-                while (fread(tmp, 1, 1024, file) == 1024) {
-                    sendto(sockfd, tmp, 1024, 0, (struct sockaddr*) &clientaddr, sizeof(clientaddr));
-                    
-                    /* Wait until ack. */
-                    recvfrom(sockfd, ack, 3, 0, (struct sockaddr*) &clientaddr, &len);
-                }
-                
-                sendto(sockfd, tmp, r, 0, (struct sockaddr*) &clientaddr, sizeof(clientaddr));
-                
-            }
-            else {
-                printf("The file '%s' could not be found.\n", fname);
-            }
-        }
-    }
-    return 0;
-}
+                sendto(sockfd, buffer, len + 1, 0, (struct sockaddr*) &clientaddr, sizeof(clientaddr));
+		*/
